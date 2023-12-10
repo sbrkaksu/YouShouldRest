2c2bc559685517b906670fc9887f196c5f36c522
Berk Aksu <berkaksu79@gmail.com>
2023-12-10 21:21:05 +0300

    Initial Commit

---
+++ YouShouldRest/YouShouldRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouShouldRestApp.swift
+//  YouShouldRest
+//
+//  Created by Berk Aksu on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct YouShouldRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
